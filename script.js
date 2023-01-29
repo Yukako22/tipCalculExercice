6671dd3ee6d14f1f4a372a2e90da217c91ec07b9
Alexandre Boudreau <92178093+Yukako22@users.noreply.github.com>
2023-01-29 01:48:01 +0000

    fini

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ These are all the DIV ID's you're gonna need access to 👇
 // bill input, tip input, number of people div, and per person total div
 const billInput = document.getElementById('billTotalInput')
 const tipInput = document.getElementById('tipInput')
-const numberOfPeapleDiv = document.getElementById('numberOfPeople')
-const perPersonTotal = document.getElementById('perPersonTotal')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
 
 
 // Get number of people from number of people div
@@ -41,25 +41,26 @@ const calculateBill = () => {
 
 
   // calculate the total (tip amount + bill)
-
+  const total = tipAmount + bill
 
   // calculate the per person total (total divided by number of people)
+  const perPersonTotal = total / numberOfPeople
 
 
   // update the perPersonTotal on DOM & show it to user
-
+  perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2).toLocaleString('en-US')}`
 }
 
 // ** Splits the bill between more people **
 const increasePeople = () => {
   // increment the amount of people
-
+  numberOfPeople += 1
 
   // update the DOM with the new number of people
-
+  numberOfPeopleDiv.innerText = numberOfPeople
 
   // calculate the bill based on the new number of people
-
+  calculateBill()
 }
 
 // ** Splits the bill between fewer people **
@@ -67,14 +68,16 @@ const decreasePeople = () => {
   // guard clause
   // if amount is 1 or less simply return
   // (a.k.a you can't decrease the number of people to 0 or negative!)
-
+  if (numberOfPeople <= 1) {
+    return
+  }
 
   // decrement the amount of people
-
+  numberOfPeople -= 1
 
   // update the DOM with the new number of people
-
+  numberOfPeopleDiv.innerText = numberOfPeople
 
   // calculate the bill based on the new number of people
-
+  calculateBill()
 }
